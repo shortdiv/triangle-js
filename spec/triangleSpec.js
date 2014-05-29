abe2a2638deaedb7938b9d7ce975752bd4cfa53a
Div <divya@heysparkbox.com>
2014-05-29 17:17:21 -0400

    Added function to test if all 3 sides of triangle are valid

--- spec/triangleSpec.js
+++ spec/triangleSpec.js
@@ -1,7 +1,26 @@
 describe("TRIANGLE", function(){
+
   describe("which triangle", function(){
     it("should return scalene given 3 unequal lengths", function(){
       expect(which_triangle(3,4,5)).toEqual("scalene")
+    });
+  });
+
+  describe("side is valid", function(){
+    it("should return false if one side given is not a number", function(){
+      expect(side_is_valid("hello")).toEqual(false);
+    });
+
+    it("should return false if one side is equal to or less than zero", function(){
+      expect(side_is_valid(0)).toEqual(false);
+    })
+  });
+
+  describe("triangle is valid", function(){
+    it("should return invalid if any of the sides is invalid", function(){
+      expect(triangle_is_valid(3,4,0)).toEqual("Triangle has an invalid side!");
     })
   })
-})
+
+
+});
--- src/triangle.js
+++ src/triangle.js
@@ -1,13 +1,26 @@
 function which_triangle(len1, len2, len3){
   var arr = [];
-  var sides = [len1, len2, len3];
-  for(var i=0; i<sides.length; i++){
-    if(arr.indexOf(sides[i]) == -1){
-      arr.push(sides[i]);
+  // var sides = [len1, len2, len3];
+  for(var i=0; i<arguments.length; i++){
+    if(arr.indexOf(arguments[i]) == -1){
+      arr.push(arguments[i]);
     };
   };
   return triangle_types[arr.length];
 };
+
+function side_is_valid(len){
+  return !isNaN(parseInt(len, 10)) && len > 0
+}
+
+function triangle_is_valid(len1,len2,len3){
+  for (var i=0; i<arguments.length; i++){
+    if (!side_is_valid(arguments[i])){
+      return("Triangle has an invalid side!");
+    }
+  }
+}
+
 
 var triangle_types = {
   1: "equilateral",
